d96dab94080bf5c968208d781176148c4999dd22
Garrett Hughes <garretthughes3@gmail.com>
2023-08-20 10:20:55 -0700

    add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,8 @@ export default function App({ Component, pageProps }) {
           <meta name="application-name" content="PWA App" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="Web3 Wordle" />
+          <meta name="description" content="Web3 Wordle" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -25,15 +25,15 @@ export default function App({ Component, pageProps }) {
           <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
           <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
 
-          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:url" content="https://web.com" />
           <meta name="twitter:title" content="PWA App" />
           <meta name="twitter:description" content="Best PWA App in the world" />
           <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
@@ -42,19 +42,8 @@ export default function App({ Component, pageProps }) {
           <meta property="og:title" content="PWA App" />
           <meta property="og:description" content="Best PWA App in the world" />
           <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
-
-          <!-- apple splash screen images -->
-          <!--
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
-          -->
+          <meta property="og:url" content="https://web3-wordle.vercel.app/" />
+          <meta property="og:image" content="https://web3-wordle.vercel.app/" />
       </Head>
       <Component {...pageProps} />
     </>
